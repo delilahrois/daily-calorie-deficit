40482252350376d738915c4ecb6e9ed95e8104ce
David Tran <frozenpho@gmail.com>
2021-10-17 15:19:58 -0600

    Update User class

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,8 +1,24 @@
 import { expect } from 'chai';
 import UserRepository from '../src/UserRepository';
+const userData = require('../src/data/users')
 
 describe('User Repository', () => {
+  let userRepo;
+
+
+  beforeEach(() => {
+    userRepo = new UserRepository(userData);
+  });
+
   it('should be a function', function () {
     expect(UserRepository).to.be.a('function');
-  });
+  })
+
+  it('should instantiate a new user repository', function () {
+    expect(userRepo).to.be.an.instanceOf(UserRepository);
+  })
+
 });
+  
+
+
