92cc6aa53cac3290c3e0a7985d1cffe241c6d663
David Tran <frozenpho@gmail.com>
2021-10-17 15:35:46 -0600

    Start constructor

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -12,11 +12,13 @@ describe('User Repository', () => {
 
   it('should be a function', function () {
     expect(UserRepository).to.be.a('function');
-  })
+  });
 
   it('should instantiate a new user repository', function () {
     expect(userRepo).to.be.an.instanceOf(UserRepository);
-  })
+  });
+
+  it('should ')
 
 });
   
